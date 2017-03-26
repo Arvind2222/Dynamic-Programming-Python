d8b60f1f0b1a7e07d35c4e03ffdcc772e173f3e8
Arvind Mishra <mishraarvind2222@gmail.com>
2017-03-26 14:27:50 +0530

    After converting the java FibDP to Pyhton fibDP

--- fib.py
+++ fib.py
@@ -1,16 +1,10 @@
-def fib(x):
-    if x==0:
-        return 0
-    elif x==1:
-        return 1
+# standard recursive implementation of fib
+class Fib(object):
+    def fib(self, x):
+        if x==0:
+            return 0
+        elif x==1:
+            return 1
 
-    return fib(x-1) + fib(x-2)
+        return self.fib(x-1) + self.fib(x-2)
 
-    
-
-def main():
-    print(fib(int(input("Enter the number: "))))
-
-
-if __name__ == '__main__':
-	main()
---
+++ main-1.py
@@ -0,0 +1,10 @@
+from fib import Fib
+
+
+def main():
+	f = Fib()
+	print (f.fib(int(input("Enter the number: "))))
+
+
+if __name__ == '__main__':
+	main()
